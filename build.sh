5054147b66a23b015c1a4e5a76cc5a6522e04413
Joe Vacovsky <vacovsky@gmail.com>
2020-11-08 09:57:44 -0800

    port

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ rm greenhouse.zip
 GOARCH=arm CGO_ENABLED=1 CC=arm-linux-gnueabihf-gcc GOARM=6 go build -o greenhouse
 # zip -r greenhouse.zip greenhouse static bin
 zip -r greenhouse.zip ./*
-scp -r greenhouse.zip 192.168.111.23:/home/joe/greenhouse/
+scp -r greenhouse.zip 192.168.111.23:/opt/greenhouse/
